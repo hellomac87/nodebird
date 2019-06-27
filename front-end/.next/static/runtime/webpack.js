1a348905fbaf93197c2c619ca9d970645180730f
hellomac87 <hellomac87@gmail.com>
2019-06-27 10:07:07 +0900

    React SNS 강좌 2-6. 프로필 화면 만들기

--- front-end/.next/static/runtime/webpack.js
+++ front-end/.next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5540b906961643dfa232";
+/******/ 	var hotCurrentHash = "01e3b92f37040e13fbcb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- front-end/pages/profile.js
+++ front-end/pages/profile.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { Form, Input, Button, List, Icon, Card } from "antd";
 
 const Profile = () => {
-  return <>Profile</>;
+  return (
+    <div>
+      <Form
+        style={{
+          marginBottom: "20px",
+          border: "1px solid #d9d9d9",
+          padding: "20px"
+        }}
+      >
+        <Input addonBefore="닉네임" />
+        <Button type="primary">수정</Button>
+      </Form>
+
+      <List
+        style={{ marginBottom: "20px" }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div>팔로워 목록</div>}
+        loadMore={<Button style={{ width: "100%" }}>더보기</Button>}
+        bordered
+        dataSource={["제로초1", "바보", "노드버드"]}
+        renderItem={item => (
+          <List.Item>
+            <Card actions={[<Icon key="stop" type="stop" />]}>
+              <Card.Meta description={item} />
+            </Card>
+          </List.Item>
+        )}
+      />
+      <List
+        style={{ marginBottom: "20px" }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div>팔로잉 목록</div>}
+        loadMore={<Button style={{ width: "100%" }}>더보기</Button>}
+        bordered
+        dataSource={["제로초", "바보", "노드버드"]}
+        renderItem={item => (
+          <List.Item>
+            <Card actions={[<Icon key="stop" type="stop" />]}>
+              <Card.Meta description={item} />
+            </Card>
+          </List.Item>
+        )}
+      />
+    </div>
+  );
 };
 
 export default Profile;
