33933beca340cde323cfb087bc58b5fe74dbdb1c
hellomac87 <hellomac87@gmail.com>
2019-06-28 12:13:41 +0900

    React SNS 강좌 3-5. redux devtools 사용하기

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import PropTypes from "prop-types";
 import AppLayout from "../components/AppLayout";
 
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import withRedux from "next-redux-wrapper";
 import reducer from "../reducers";
@@ -32,7 +32,14 @@ NodeBird.propTypes = {
 };
 
 export default withRedux((initialState, options) => {
-  const store = createStore(reducer, initialState);
-  // store customizing
+  const middlewares = [];
+  const enhancer = compose(
+    applyMiddleware(...middlewares),
+    !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  );
+  const store = createStore(reducer, initialState, enhancer);
+
   return store;
 })(NodeBird);
