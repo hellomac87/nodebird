b903707d426973b8fa2c83f8be967b4f303a3faa
hellomac87 <hellomac87@gmail.com>
2019-07-29 15:04:52 +0900

    React SNS 강좌 4-2. 사가 미들웨어 리덕스에 연결하기

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -7,6 +7,8 @@ import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import withRedux from "next-redux-wrapper";
 import reducer from "../reducers";
+import sagaMiddleware from "../sagas/middleware";
+import rootSaga from "../sagas";
 
 const NodeBird = ({ Component, store }) => {
   return (
@@ -32,14 +34,19 @@ NodeBird.propTypes = {
 };
 
 export default withRedux((initialState, options) => {
-  const middlewares = [];
-  const enhancer = compose(
-    applyMiddleware(...middlewares),
-    !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : f => f
-  );
+  const middlewares = [sagaMiddleware];
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : compose(
+          applyMiddleware(...middlewares),
+          !options.isServer &&
+            window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
+            ? window.__REDUX_DEVTOOLS_EXTENSION__()
+            : f => f
+        );
   const store = createStore(reducer, initialState, enhancer);
+  sagaMiddleware.run(rootSaga);
 
   return store;
 })(NodeBird);
