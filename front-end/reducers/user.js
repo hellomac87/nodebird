99757659f9878c94a8b104001e5af7abb5b6c8d6
hellomac87 <hellomac87@gmail.com>
2019-07-29 12:07:18 +0900

    React SNS 강좌 4-1. 리덕스 사가의 필요성과 맛보기

--- front-end/reducers/user.js
+++ front-end/reducers/user.js
@@ -19,8 +19,11 @@ export const initialState = {
 
 // action constatns
 export const SIGN_UP = "SIGN_UP";
-const LOG_IN = "LOG_IN";
-const LOG_OUT = "LOG_OUT";
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const LOG_IN = "LOG_IN";
+export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
+export const LOG_IN_FAILURE = "LOG_IN_FAILURE";
+export const LOG_OUT = "LOG_OUT";
 
 export const signupAction = data => {
   return {
